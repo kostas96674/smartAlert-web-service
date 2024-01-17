ffbe568e37d60eee42a0ea7aa7ed8aad048ab403
dimitrisstyl7 <dimitrisstylianou@proton.me>
2024-01-17 19:46:01 +0200

    Create smartAlert-web-service project
    Co-Authored-By: Anthony Roussos <79643636+anthonyrouss@users.noreply.github.com>
    Co-Authored-By: KONSTANTINOS LOIZIDIS <79859276+kostas96674@users.noreply.github.com>

---
+++ SmartAlertAPI/src/main/java/com/unipi/smartalert/SmartAlertApiApplication.java
@@ -0,0 +1,13 @@
+package com.unipi.smartalert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartAlertApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartAlertApiApplication.class, args);
+    }
+
+}
