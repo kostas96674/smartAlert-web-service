ccdd0662b5686dc05e8f1c9fc0ff3a0f18ec42ce
Anthony Roussos <github.eloquent385@passinbox.com>
2024-01-19 23:38:25 +0200

    Create Roles Controller

---
+++ SmartAlertAPI/src/main/java/com/unipi/smartalert/controllers/RolesController.java
@@ -0,0 +1,39 @@
+package com.unipi.smartalert.controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/roles")
+public class RolesController {
+
+    // GET /roles - Get all roles
+    @GetMapping
+    public void getAll() {
+        // TODO: Implement
+    }
+
+    // GET /roles/{id} - Get a role by ID
+    @GetMapping("/{id}")
+    public void getOneById(@PathVariable int id) {
+        // TODO: Implement
+    }
+
+    // POST /roles - Create a new role
+    @PostMapping
+    public void create() {
+        // TODO: Implement
+    }
+
+    // PUT /roles/{id} - Update a role
+    @PutMapping("/{id}")
+    public void update(@PathVariable int id) {
+        // TODO: Implement
+    }
+
+    // DELETE /roles/{id} - Delete a role
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable int id) {
+        // TODO: Implement
+    }
+
+}
