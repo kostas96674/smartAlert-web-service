5b88f09f868325bfaeb43b88f020418bf3df926b
Anthony Roussos <github.eloquent385@passinbox.com>
2024-02-28 01:30:35 +0200

    Create IncidentCategory DTO
    Create a DTO for IncidentCategory to include the information we want to send to the client.

---
+++ SmartAlertAPI/src/main/java/com/unipi/smartalert/dtos/IncidentCategoryDTO.java
@@ -0,0 +1,15 @@
+package com.unipi.smartalert.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class IncidentCategoryDTO {
+
+    private Long id;
+    private String name;
+
+}
