3549db14b278d280fd82677f422526bee8df4fd6
dimitrisstyl7 <dimitrisstylianou@proton.me>
2024-02-28 20:53:13 +0200

    Update IncidentCategoryServiceImpl.java
    - Add findById method.

--- SmartAlertAPI/src/main/java/com/unipi/smartalert/services/impl/IncidentCategoryServiceImpl.java
+++ SmartAlertAPI/src/main/java/com/unipi/smartalert/services/impl/IncidentCategoryServiceImpl.java
@@ -2,6 +2,7 @@ package com.unipi.smartalert.services.impl;
 
 import com.unipi.smartalert.dtos.IncidentCategoryDTO;
 import com.unipi.smartalert.exceptions.LanguageNotSupportedException;
+import com.unipi.smartalert.models.IncidentCategory;
 import com.unipi.smartalert.repositories.IncidentCategoryRepository;
 import com.unipi.smartalert.services.IncidentCategoryService;
 import lombok.AllArgsConstructor;
@@ -18,8 +19,15 @@ public class IncidentCategoryServiceImpl implements IncidentCategoryService {
     @Override
     public List<IncidentCategoryDTO> findByLanguage(String language) {
         var incidentCategoryDTOList = repository.findByLanguage(language);
-        if (incidentCategoryDTOList.isEmpty()) throw new LanguageNotSupportedException("Language '" + language + "' is not supported");
+        if (incidentCategoryDTOList.isEmpty())
+            throw new LanguageNotSupportedException("Language '" + language + "' is not supported");
         return incidentCategoryDTOList;
+    }
+
+    @Override
+    public IncidentCategory findById(long id) {
+        // Todo: Throw RecourseNotFoundException
+        return repository.findById(id).orElseThrow(() -> new RuntimeException("Incident category with id " + id + " not found"));
     }
 
 }
