612624fa9c849901ffe62a7c638cbef10bb70f0c
dimitrisstyl7 <dimitrisstylianou@proton.me>
2024-02-28 20:51:23 +0200

    Update IncidentCategoryService.java
    - Add findById method.

--- SmartAlertAPI/src/main/java/com/unipi/smartalert/services/IncidentCategoryService.java
+++ SmartAlertAPI/src/main/java/com/unipi/smartalert/services/IncidentCategoryService.java
@@ -1,12 +1,14 @@
 package com.unipi.smartalert.services;
 
 import com.unipi.smartalert.dtos.IncidentCategoryDTO;
-import org.springframework.stereotype.Service;
+import com.unipi.smartalert.models.IncidentCategory;
 
 import java.util.List;
 
 public interface IncidentCategoryService {
 
     List<IncidentCategoryDTO> findByLanguage(String language);
+
+    IncidentCategory findById(long id);
 
 }
