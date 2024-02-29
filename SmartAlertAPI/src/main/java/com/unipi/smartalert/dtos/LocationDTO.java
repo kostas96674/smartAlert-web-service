87574fdf0c0db8ea5636249ddd6d3479dadcfaee
Anthony Roussos <github.eloquent385@passinbox.com>
2024-02-29 15:19:31 +0200

    Add Builder annotation

--- SmartAlertAPI/src/main/java/com/unipi/smartalert/dtos/LocationDTO.java
+++ SmartAlertAPI/src/main/java/com/unipi/smartalert/dtos/LocationDTO.java
@@ -1,10 +1,12 @@
 package com.unipi.smartalert.dtos;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class LocationDTO {
