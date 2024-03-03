7f3d8e2271c8d7853172d27b82129277b24896b3
Anthony Roussos <github.eloquent385@passinbox.com>
2024-03-04 01:21:18 +0200

    Update ReportDTO.java

--- SmartAlertAPI/src/main/java/com/unipi/smartalert/dtos/ReportDTO.java
+++ SmartAlertAPI/src/main/java/com/unipi/smartalert/dtos/ReportDTO.java
@@ -1,15 +1,22 @@
 package com.unipi.smartalert.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class ReportDTO {
 
-    Long categoryId;
-    LocationDTO location;
-    String timestamp;
-    String description;
+    private Long id;
+    private Long categoryId;
+    private LocationDTO location;
+    private String timestamp;
+    private String description;
+    private UserDTO sender;
+    private boolean hasImage;
 
 }
