ca2abf5e9543fc03254182795f5efa7311b269bf
Anthony Roussos <github.eloquent385@passinbox.com>
2024-03-04 22:58:15 +0200

    Update UserService.java

--- SmartAlertAPI/src/main/java/com/unipi/smartalert/services/UserService.java
+++ SmartAlertAPI/src/main/java/com/unipi/smartalert/services/UserService.java
@@ -6,5 +6,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 public interface UserService extends UserDetailsService {
 
     User findByEmail(String email);
+    void save(User user);
 
 }
