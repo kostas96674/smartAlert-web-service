c24b0e3ba35998fe88f67bab867c58f82d670b4a
Anthony Roussos <github.eloquent385@passinbox.com>
2024-03-04 23:03:23 +0200

    Create sendMessageAsync method
    Create sendMessageAsync method to send a message using Firebase Cloud Messaging service.

--- SmartAlertAPI/src/main/java/com/unipi/smartalert/services/FirebaseService.java
+++ SmartAlertAPI/src/main/java/com/unipi/smartalert/services/FirebaseService.java
@@ -1,6 +1,7 @@
 package com.unipi.smartalert.services;
 
 import com.unipi.smartalert.dtos.ReportGroupDTO;
+import com.unipi.smartalert.dtos.IncidentMessageDTO;
 import com.unipi.smartalert.listeners.APIResponseListener;
 import lombok.NonNull;
 
@@ -8,5 +9,6 @@ public interface FirebaseService {
 
     void writeToDatabaseAsync(ReportGroupDTO reportGroupDTO, @NonNull APIResponseListener<Void> listener);
     void removeGroupFromDatabaseAsync(long groupId, @NonNull APIResponseListener<Void> listener);
+    void sendMessageAsync(IncidentMessageDTO incidentMessageDTO, @NonNull APIResponseListener<Void> listener);
 
 }
